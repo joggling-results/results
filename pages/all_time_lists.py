c5dca59848dec2b0591a6b58e4c0019a98bd793d
Scott Jenkins <sjenkins097@gmail.com>
2023-05-06 19:26:05 +0100

    doh

--- pages/all_time_lists.py
+++ pages/all_time_lists.py
@@ -17,7 +17,7 @@ data = pd.read_csv('test_results.csv')    ## xlsx not supported.
 def all_time_list(distance):
     fastest_times = data[data['Distance']==distance][['Joggler','Finish Time']].groupby(['Joggler']).min().reset_index()
     fastest_times['Ranking'] = pd.to_numeric(fastest_times['Finish Time'].rank(method="dense")).astype(int)
-    fastest_times[['Ranking','Joggler','Finish Time']].sort_values('Ranking').reset_index(drop=True)
+    fastest_times = fastest_times[['Ranking','Joggler','Finish Time']].sort_values('Ranking').reset_index(drop=True)
     return fastest_times
 
 st.write(all_time_list('3b Mile'))
