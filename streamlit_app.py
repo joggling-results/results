7f4d27b75ed8389548eedeea864a4a3896aaeb1e
Scott Jenkins <sjenkins097@gmail.com>
2023-05-06 18:05:24 +0100

    another test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,16 @@
 import pandas as pd
+import os
 import streamlit
+
+print('all loaded')
+
+
+
+# st.set_page_config(page_title='Joggling', page_icon=':zap:')
+
+cwd = os.getcwd()
+print(cwd)
+# data = pd.read_excel('joggling_backup_300423.xlsx')
+
+# data.head()
+
