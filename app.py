9b393e37ae437155c704d59fea0304b252eb5e18
Scott Jenkins <sjenkins097@gmail.com>
2023-05-06 18:27:17 +0100

    text

--- app.py
+++ app.py
@@ -7,11 +7,16 @@ import os
 # set up app
 st.set_page_config(page_title='Joggling', page_icon=':zap:')
 st.title('Joggling Results Archive')
-st.subtitle('Scott Jenkins & Chris Edwin')
+st.subheader('Curated by Scott Jenkins & Chris Edwin')
+
+st.write("""Joggling (that is, the hybrid sport of running whilst juggling) is certainly a niche.
+This web app aims to present an archive of 500+ joggling results, achieved by 200+ jogglers.
+Feedback and Suggestions to jogglingresults@gmail.com
+""")
+
 
 cwd = os.getcwd()
 print(cwd)
-
 
 # data = pd.read_excel('joggling_backup_300423.xlsx')
 # print(data.head())
