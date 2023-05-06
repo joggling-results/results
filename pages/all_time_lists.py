bfeaa3f17d2a5ead742c39b4e1a12184709e52fe
Scott Jenkins <sjenkins097@gmail.com>
2023-05-06 19:44:25 +0100

    t

--- pages/all_time_lists.py
+++ pages/all_time_lists.py
@@ -23,16 +23,16 @@ def all_time_list(distance):
     fastest_times = fastest_times[['Ranking','Joggler','Gender','Nationality','Date','Event / Venue','Finish Time']].sort_values('Ranking').reset_index(drop=True)
     return fastest_times
 
-tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8 = st.tabs(["3b Mile", "3b 5km", "3b 10km",'3b Half Marathon', '3b Marathon','5b Mile','5b 5km','5b Marathon'])
+tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8 = st.tabs(["3b Mile", "3b 5km", "3b 10km",'3b Half Marathon', '3b Marathon', '5b Mile', '5b 5km','5b Marathon'])
 
 with tab1:
    st.header("3 Ball Mile")
    st.write(all_time_list('3b Mile'))
 with tab2:
-   st.header("3 Ball 5km")
+   st.subheader("3 Ball 5km")
    st.write(all_time_list('3b 5km'))
 with tab3:
-   st.header("3 Ball 10km")
+   st.subheader("3 Ball 10km")
    st.write(all_time_list('3b 10km'))
 with tab4:
    st.header("3 Ball Half Marathon")
