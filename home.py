26183477a8a4229f37f427ed6eafcde7f713c4ce
Scott Jenkins <sjenkins097@gmail.com>
2023-05-06 18:48:25 +0100

    rename

---
+++ home.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import os
+
+
+
+# set up app
+st.set_page_config(page_title='Joggling', page_icon=':zap:')
+st.title('Joggling Results Archive')
+st.subheader('Curated by Scott Jenkins & Chris Edwin')
+
+st.write("""Joggling (that is, the hybrid sport of running whilst juggling) is certainly a niche.
+This web app aims to present an archive of 500+ joggling results, achieved by 200+ jogglers.
+Feedback and Suggestions to jogglingresults@gmail.com
+""")
+
+
+cwd = os.getcwd()
+print(cwd)
+
+data = pd.read_csv('test_results.csv')    ## xlsx not supported.
+st.write(data)
+
