3954c553701c66d359cfef16ef596198ad1379ad
Scott Jenkins <sjenkins097@gmail.com>
2023-05-06 18:11:21 +0100

    another commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,13 @@ import pandas as pd
 import os
 import streamlit
 
-print('all loaded')
-
-
-
-# st.set_page_config(page_title='Joggling', page_icon=':zap:')
+# set up app
+st.set_page_config(page_title='Joggling', page_icon=':zap:')
 
 cwd = os.getcwd()
 print(cwd)
-data = pd.read_excel('joggling_backup_300423.xlsx')
 
+
+data = pd.read_excel('joggling_backup_300423.xlsx')
 print(data.head())
 
