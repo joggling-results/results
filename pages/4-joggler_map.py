c2589b912e8800d4e1215fd6892284400a6aff2f
Scott Jenkins <sjenkins097@gmail.com>
2023-05-07 18:40:53 +0100

    what

--- pages/4-joggler_map.py
+++ pages/4-joggler_map.py
@@ -3,15 +3,30 @@ import pandas as pd
 from datetime import datetime
 import plotly.express as px
 
+
+## Example bar chart
+data = {'x': ['A', 'B', 'C', 'D'],
+        'y': [10, 20, 30, 40]}
+
+# Convert data to DataFrame
+df = pd.DataFrame(data)
+
+# Create the bar chart
+my_bar = px.bar(df, x='x', y='y')
+
+st.plotly_chart(my_bar)
+
+
+## Example Map
 data = {'country_code': ['GBR', 'USA', 'CAN', 'AUS'],
         'units': [100, 500, 250, 150]}
 df = pd.DataFrame(data)
 
 # create map with dot size representing number of units
-fig = px.scatter_geo(df, locations="country_code", size="units",
+my_map = px.scatter_geo(df, locations="country_code", size="units",
                      projection="natural earth", hover_name='country_code',size_max = 50)
 
 # Display map in Streamlit app
-st.plotly_chart(fig)
+st.plotly_chart(my_map)
 
 
