d759de9c98cbb951e137037a8e7e9a4ab35db1dd
Scott Jenkins <sjenkins097@gmail.com>
2023-05-07 18:10:07 +0100

    Adding Filters

--- home.py
+++ home.py
@@ -32,18 +32,6 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
         return df
 
     df = df.copy()
-
-    # Try to convert datetimes into a standard format (datetime, no timezone)
-    # for col in df.columns:
-    #     if is_object_dtype(df[col]):
-    #         try:
-    #             df[col] = pd.to_datetime(df[col])
-    #         except Exception:
-    #             pass
-
-    #     if is_datetime64_any_dtype(df[col]):
-    #         df[col] = df[col].dt.tz_localize(None)
-
     modification_container = st.container()
 
     with modification_container:
--- pages/3-all_time_lists.py
+++ pages/3-all_time_lists.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
 
 st.set_page_config(page_title='All Time Lists',
                    page_icon=':rocket:',
@@ -8,8 +14,72 @@ st.set_page_config(page_title='All Time Lists',
                    initial_sidebar_state = 'expanded'   ## 'auto','collapsed','expanded'
                    )
 
-# Use cwd to ensure runs on all machines
+## Function to allow dataframe filtering
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds a UI on top of a dataframe to let viewers filter columns
 
+    Args:
+        df (pd.DataFrame): Original dataframe
+
+    Returns:
+        pd.DataFrame: Filtered dataframe
+    """
+    modify = st.checkbox("Add filters")
+
+    if not modify:
+        return df
+
+    df = df.copy()
+    modification_container = st.container()
+
+    with modification_container:
+        to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
+        for column in to_filter_columns:
+            left, right = st.columns((1, 20))
+            # Treat columns with < 10 unique values as categorical
+            if is_categorical_dtype(df[column]) or df[column].nunique() < 20:
+                user_cat_input = right.multiselect(
+                    f"Values for {column}",
+                    df[column].unique(),
+                    default=list(df[column].unique()),
+                )
+                df = df[df[column].isin(user_cat_input)]
+            elif is_numeric_dtype(df[column]):
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                user_num_input = right.slider(
+                    f"Values for {column}",
+                    min_value=_min,
+                    max_value=_max,
+                    value=(_min, _max),
+                    step=step,
+                )
+                df = df[df[column].between(*user_num_input)]
+            elif is_datetime64_any_dtype(df[column]):
+                user_date_input = right.date_input(
+                    f"Values for {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Substring or regex in {column}",
+                )
+                if user_text_input:
+                    df = df[df[column].astype(str).str.contains(user_text_input)]
+
+    return df
+## End of dataframe filtering function
+
+## Start of Page Content
 st.markdown('#### All-Time Lists')
 
 st.write("Use the tabs below to see the fastest jogglers in different events")
@@ -29,7 +99,7 @@ tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8 = st.tabs(["3b Mile", "3b 5km", "
 
 with tab1:
    st.subheader("3 Ball Mile")
-   st.write(all_time_list('3b Mile'))
+   st.write(filter_dataframe(all_time_list('3b Mile')))
 with tab2:
    st.subheader("3 Ball 5km")
    st.write(all_time_list('3b 5km'))
