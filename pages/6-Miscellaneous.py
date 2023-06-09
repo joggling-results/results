421e7af5bf7a3fbd66acfbbc0bcaabe5f877b739
Scott Jenkins <sjenkins097@gmail.com>
2023-06-09 22:00:35 +0100

    update

--- pages/6-Miscellaneous.py
+++ pages/6-Miscellaneous.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.write('A place for miscellaneous results, and the official Guinness World Records.')
+st.write('WIP: A place for miscellaneous results, and the official Guinness World Records.')
