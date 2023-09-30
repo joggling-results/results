811bcc3da4d6d570a17cadd841dc7d51a48b454e
Joggling Results <jogglingresults@gmail.com>
2023-09-30 16:17:52 +0100

    Data update

--- home.py
+++ home.py
@@ -15,7 +15,7 @@ st.set_page_config(page_title='Joggling',
                    initial_sidebar_state = 'expanded'   ## 'auto','collapsed','expanded'
                    )
 
-update_date = '25th September 2023'
+update_date = '30th September 2023'
 
 ## Function to allow dataframe filtering
 def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
